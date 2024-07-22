38071fcb4498c68d23bc2252588edc4e421fdac4
Agash. A <141730950+Agash1903@users.noreply.github.com>
2024-07-22 16:49:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function changea(){
+ var color=document.getElementById('a');
+ color.style.backgroundColor='green';
+    
+
+ }
+function changeb(){
+    document.getElementById('b').style.backgroundColor='red';
+
+}
+function changec(){
+    document.getElementById('c').style.backgroundColor='red';
+
+}
+function changed(){
+    document.getElementById('d').style.backgroundColor='red';
+
+}
+
+
